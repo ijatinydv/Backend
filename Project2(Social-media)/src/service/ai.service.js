8333d8772cb070b139335f2f49ef0a2da3394189
Jatin Yadav <heroraoshab2005@gmail.com>
2025-09-01 23:11:26 +0530

    project completed

--- Project2(Social-media)/src/service/ai.service.js
+++ Project2(Social-media)/src/service/ai.service.js
@@ -1,16 +1,28 @@
-const { GoogleGenAI } = require( "@google/genai")
+const { GoogleGenAI } = require("@google/genai");
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({
-    apiKey:""
-});
+const ai = new GoogleGenAI({});
 
-async function main() {
+async function generateCaption(base64ImageFile) {  // this is called abstration -> process of hiding complex implementation details and showing only the necessary part of an function to the developer.
+  const contents = [
+    {
+      inlineData: {
+        mimeType: "image/jpeg",
+        data: base64ImageFile,
+      },
+    },
+    { text: "Caption this image." },
+  ];
+
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
+    contents: contents,
+    config:{
+      systemInstruction: `You are expert in generating captions for images. You generate single caption for an image. Your caption should be short and concise. You use hashtags and emojis in the caption.`
+      // you can guide the ai what to do
+    }
   });
-  console.log(response.text);
+  return response.text;
 }
 
-main();
+module.exports = generateCaption
---
+++ Project2(Social-media)/src/service/storage.service.js
@@ -0,0 +1,18 @@
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint : "https://ik.imagekit.io/your_imagekit_id/"
+});
+
+async function uploadFile(file,filename) {
+    const response = await imagekit.upload({
+        file:file,
+        fileName:filename,
+        folder: cohort-ai-social
+    })
+    return response;
+}
+
+module.exports = uploadFile;
